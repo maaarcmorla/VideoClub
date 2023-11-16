53509f1a2600880f542844585bf58f9664f42107
Hugo Serra Ruiz <hugoserraruiz@gmail.com>
2023-11-16 12:20:49 +0100

    Implementada la clase Juego

---
+++ Juego.php
@@ -0,0 +1,27 @@
+<?php
+class Juego extends Soporte{
+    public $consola;
+    private $minNumJugadores;
+    private $maxNumJugadores;
+
+    function __construct($titulo, $numero, $precio, $consola, $minNumJugadores, $maxNumJugadores){  
+        parent::__construct($titulo, $numero, $precio);
+        $this->consola = $consola;
+        $this->minNumJugadores = $minNumJugadores;
+        $this->maxNumJugadores = $maxNumJugadores;     
+    }
+    function muestraJugadoresPosibles(){
+        if ($this->maxNumJugadores ==1){
+            echo ("Para un jugador");
+        }else{
+            echo ("Para ".$this->maxNumJugadores." jugadores");
+        }
+    }
+    function muestraResumen(){
+        echo ("<br>Juego para: ".$this->consola);
+        parent::muestraResumen();
+        self::muestraJugadoresPosibles();
+        
+    }
+}
+?>
