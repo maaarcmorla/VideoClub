4489103ffa9e00e2006b4db94e99b6fdf116cc08
Marc Morlá Isern <marc.s320928@cesurformacion.com>
2023-11-18 10:48:17 +0100

    Codigo Comentado CintaVideo.php

--- CintaVideo.php
+++ CintaVideo.php
@@ -2,11 +2,13 @@
 class CintaVideo extends Soporte{
     private $duracion;
 
+    // Constructor CintaVideo
     function __construct($titulo, $numero, $precio, $duracion){  
         parent::__construct($titulo, $numero, $precio);
         $this->duracion = $duracion;   
     }
 
+    // Muestra el resumen que hereda de la clase Soporte
     function muestraResumen(){
         echo ("<br>Pelicula en VHS:");
         parent::muestraResumen();
